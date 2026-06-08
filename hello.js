1c5626cda468f9f7bdf0abc41b3d7b9b88ba0526
jeetsalunke7283-cmyk <jeetsalunke7283@gmail.com>
2026-06-08 11:44:27 +0530

    hello code

---
+++ hello.js
@@ -0,0 +1,14 @@
+let a = "Ayush";
+let b = "Amogh";
+console.log(`The name is ${a}`);
+console.log(`The name is ${b}`);
+
+class Laptop {
+
+    b = "Ayush has better game sense than Amogh";
+
+     constructor() {
+        console.log(`This is the truth ${this.b}`);
+    }
+}
+new Laptop();
